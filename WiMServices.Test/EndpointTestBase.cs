6fc32ea6ee8a64ead3a66cfe53087d7d09a0e0e4
jknewson <jknewson@usgs.gov>
2015-09-30 12:56:06 -0600

    Added try catch to codects, added expression ops and mathOps to library

--- WiMServices.Test/EndpointTestBase.cs
+++ WiMServices.Test/EndpointTestBase.cs
@@ -10,6 +10,8 @@ using OpenRasta.Web;
 using Newtonsoft.Json;
 using OpenRasta.Configuration;
 
+//https://www.danielirvine.com/blog/2011/06/08/testing-restful-services-with-openrasta/
+
 namespace WiMServices.Test
 {
     public abstract class EndpointTestBase
@@ -32,10 +34,50 @@ namespace WiMServices.Test
                     Uri = new Uri(url),
                     HttpMethod = "GET"
                 };
-                // set up your code formats - I'm using
-                // JSON because it's awesome
+                // set up your code formats
                 request.Entity.ContentType = MediaType.Json;
                 request.Entity.Headers["Accept"] = "application/json";
+
+                // send the request and save the resulting response
+                var response = host.ProcessRequest(request);
+                int statusCode = response.StatusCode;
+
+                // deserialize the content from the response
+
+                if (response.Entity.ContentLength > 0)
+                {
+                    // you must rewind the stream
+                    response.Entity.Stream.Seek(0, SeekOrigin.Begin);
+
+                    JsonSerializer serializer = new JsonSerializer();
+                    using (StreamReader streamReader = new StreamReader(response.Entity.Stream, new UTF8Encoding(false, true)))
+                    {
+                        using (JsonTextReader jsonTextReader = new JsonTextReader(streamReader))
+                        {
+                            return serializer.Deserialize<T>(jsonTextReader);
+                        }//end using
+                    }//end using
+                }//end if
+            }//end using  
+            return default(T);
+        }
+        protected T POSTRequest<T>(string url, T content)
+        {
+            using (InMemoryHost host = new InMemoryHost(ConfigSource))
+            {
+                var request = new InMemoryRequest()
+                {
+                    Uri = new Uri(url),
+                    HttpMethod = "POST"
+                };
+                // set up your code formats
+                request.Entity.ContentType = MediaType.Json;
+                request.Entity.Headers["Accept"] = "application/json";
+
+                //JsonSerializer serializer1 = new JsonSerializer(typeof(T));
+                //serializer1.Serialize(request.Entity.Stream, content);
+                //request.Entity.Stream.Seek(0, SeekOrigin.Begin);
+                //request.Entity.ContentLength = Request.Entity.Stream.Length;
 
                 // send the request and save the resulting response
                 var response = host.ProcessRequest(request);
@@ -61,5 +103,6 @@ namespace WiMServices.Test
             }//end using  
             return default(T);
         }
-    }
-}
+
+    }//end class
+}//end namespace
