fa8726cd658bf622c67009fcbdcc115e9dc79e83
jknewson <jknewson@usgs.gov>
2016-04-12 10:55:39 -0600

    added password hashing and unit tests, also modified jsonserializer preferences

--- WiMServices.Test/EndpointTestBase.cs
+++ WiMServices.Test/EndpointTestBase.cs
@@ -44,23 +44,8 @@ namespace WiM.Test
                 int statusCode = response.StatusCode;
 
                 // deserialize the content from the response
-
-                if (response.Entity.ContentLength > 0)
-                {
-                    // you must rewind the stream
-                    response.Entity.Stream.Seek(0, SeekOrigin.Begin);
-
-                    JsonSerializer serializer = new JsonSerializer();
-                    using (StreamReader streamReader = new StreamReader(response.Entity.Stream, new UTF8Encoding(false, true)))
-                    {
-                        using (JsonTextReader jsonTextReader = new JsonTextReader(streamReader))
-                        {
-                            return serializer.Deserialize<T>(jsonTextReader);
-                        }//end using
-                    }//end using
-                }//end if
-            }//end using  
-            return default(T);
+                return deserialize<T>(response);    
+            }//end using 
         }
         protected T POSTRequest<T>(string url, T content,string authenticationHeader = "")
         {
@@ -75,47 +60,14 @@ namespace WiM.Test
                 request.Entity.ContentType = MediaType.Json;
                 request.Entity.Headers["Accept"] = "application/json";
                 if (!string.IsNullOrEmpty(authenticationHeader)) request.Entity.Headers["Authorization"] = authenticationHeader;
-
-                using (JsonTextWriter jsonTextWriter = new JsonTextWriter(new StreamWriter(request.Entity.Stream, new UTF8Encoding(false, true))) { CloseOutput = false }) {
-                    JsonSerializer serializer = new JsonSerializer();
-                    //serializer.PreserveReferencesHandling = PreserveReferencesHandling.None;
-                    //serializer.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                    //serializer.MissingMemberHandling = MissingMemberHandling.Ignore;
-                    serializer.NullValueHandling = NullValueHandling.Ignore;
-                    serializer.TypeNameHandling = TypeNameHandling.None;
-                    serializer.TypeNameAssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple;
-                    serializer.Formatting = Formatting.None;
-
-                    serializer.Serialize(jsonTextWriter, content);
-                    jsonTextWriter.Flush();
-                    request.Entity.ContentLength = request.Entity.Stream.Length;
-                    //rewinding the stream.
-                    request.Entity.Stream.Seek(0, SeekOrigin.Begin);
-                }//end using
-
+                this.serialize(ref request, content);
                 // send the request and save the resulting response
                 var response = host.ProcessRequest(request);
                 int statusCode = response.StatusCode;
 
                 // deserialize the content from the response
-
-                if (response.Entity.ContentLength > 0)
-                {
-                    // you must rewind the stream, as OpenRasta
-                    // won't do this for you
-                    response.Entity.Stream.Seek(0, SeekOrigin.Begin);
-
-                    JsonSerializer serializer = new JsonSerializer();
-                    using (StreamReader streamReader = new StreamReader(response.Entity.Stream, new UTF8Encoding(false, true)))
-                    {
-                        using (JsonTextReader jsonTextReader = new JsonTextReader(streamReader))
-                        {
-                            return serializer.Deserialize<T>(jsonTextReader);
-                        }//end using
-                    }//end using
-                }//end if
-            }//end using  
-            return default(T);
+                return deserialize<T>(response);                
+            }//end using
         }
         protected T PUTRequest<T>(string url, T content, string authenticationHeader = "")
         {
@@ -130,48 +82,15 @@ namespace WiM.Test
                 request.Entity.ContentType = MediaType.Json;
                 request.Entity.Headers["Accept"] = "application/json";
                 if (!string.IsNullOrEmpty(authenticationHeader)) request.Entity.Headers["Authorization"] = authenticationHeader;
-
-                using (JsonTextWriter jsonTextWriter = new JsonTextWriter(new StreamWriter(request.Entity.Stream, new UTF8Encoding(false, true))) { CloseOutput = false })
-                {
-                    JsonSerializer serializer = new JsonSerializer();
-                    //serializer.PreserveReferencesHandling = PreserveReferencesHandling.None;
-                    //serializer.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-                    //serializer.MissingMemberHandling = MissingMemberHandling.Ignore;
-                    serializer.NullValueHandling = NullValueHandling.Ignore;
-                    serializer.TypeNameHandling = TypeNameHandling.None;
-                    serializer.TypeNameAssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple;
-                    serializer.Formatting = Formatting.None;
-
-                    serializer.Serialize(jsonTextWriter, content);
-                    jsonTextWriter.Flush();
-                    request.Entity.ContentLength = request.Entity.Stream.Length;
-                    //rewinding the stream.
-                    request.Entity.Stream.Seek(0, SeekOrigin.Begin);
-                }//end using
+                this.serialize(ref request, content);
 
                 // send the request and save the resulting response
                 var response = host.ProcessRequest(request);
                 int statusCode = response.StatusCode;
 
                 // deserialize the content from the response
-
-                if (response.Entity.ContentLength > 0)
-                {
-                    // you must rewind the stream, as OpenRasta
-                    // won't do this for you
-                    response.Entity.Stream.Seek(0, SeekOrigin.Begin);
-
-                    JsonSerializer serializer = new JsonSerializer();
-                    using (StreamReader streamReader = new StreamReader(response.Entity.Stream, new UTF8Encoding(false, true)))
-                    {
-                        using (JsonTextReader jsonTextReader = new JsonTextReader(streamReader))
-                        {
-                            return serializer.Deserialize<T>(jsonTextReader);
-                        }//end using
-                    }//end using
-                }//end if
+                return this.deserialize<T>(response);
             }//end using  
-            return default(T);
         }
         protected Boolean DELETERequest<T>(string url, string authenticationHeader = "")
         {
@@ -192,11 +111,50 @@ namespace WiM.Test
                 var response = host.ProcessRequest(request);
                 statusCode = response.StatusCode;
 
-                // deserialize the content from the response
-
             }//end using  
             return statusCode == 200;
         }
 
+
+        private void serialize(ref InMemoryRequest request, object content ) {
+            using (JsonTextWriter jsonTextWriter = new JsonTextWriter(new StreamWriter(request.Entity.Stream, new UTF8Encoding(false, true))) { CloseOutput = false })
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.PreserveReferencesHandling = PreserveReferencesHandling.None;
+                serializer.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                serializer.MissingMemberHandling = MissingMemberHandling.Ignore;
+                serializer.NullValueHandling = NullValueHandling.Ignore;
+                serializer.TypeNameHandling = TypeNameHandling.None;
+                serializer.TypeNameAssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple;
+                serializer.Formatting = Formatting.None;
+
+                serializer.Serialize(jsonTextWriter, content);
+                jsonTextWriter.Flush();
+                request.Entity.ContentLength = request.Entity.Stream.Length;
+                //rewinding the stream.
+                request.Entity.Stream.Seek(0, SeekOrigin.Begin);
+            }//end using
+        
+        
+        }
+        private T deserialize<T>(IResponse response) {
+            if (response.Entity.ContentLength > 0)
+            {
+                // you must rewind the stream, as OpenRasta
+                // won't do this for you
+                response.Entity.Stream.Seek(0, SeekOrigin.Begin);
+
+                JsonSerializer serializer = new JsonSerializer();
+                using (StreamReader streamReader = new StreamReader(response.Entity.Stream, new UTF8Encoding(false, true)))
+                {
+                    using (JsonTextReader jsonTextReader = new JsonTextReader(streamReader))
+                    {
+                        return serializer.Deserialize<T>(jsonTextReader);
+                    }//end using
+                }//end using
+            }//end if
+            return default(T);
+        
+        }
     }//end class
 }//end namespace
