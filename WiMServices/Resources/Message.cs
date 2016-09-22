3581731cacbc20cf07191debfdd755e5ee7bd95e
jknewson <jknewson@usgs.gov>
2016-09-22 08:48:15 -0600

    restructure messages

--- WiMServices/Resources/Message.cs
+++ WiMServices/Resources/Message.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 namespace WiM.Resources
 {
+    public interface IMessage
+    {
+        List<Message> Messages { get;}
+    }
     public struct Message
     {
         public MessageType type { get; set; }
-        public string msg { get; set; }
-
+        public string msg { get; set; }        
     }
     public enum MessageType
     {
