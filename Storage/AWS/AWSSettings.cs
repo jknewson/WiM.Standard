73ffb871c58e5ca23cf66ab08abbbf96e62d0dd1
Jeremy Newson <jknewson@usgs.gov>
2019-05-16 09:05:11 -0600

    Added storage

---
+++ Storage/AWS/AWSSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIM.Storage.AWS
+{
+    public class AWSSettings
+    {
+        public string BucketName { get; set; }
+        public string Key { get; set; }
+        public string SecretKey { get; set; }
+        /// <summary>
+        /// Specified region name, like "***REMOVED***"
+        /// </summary>
+        public string RegionName { get; set; } = "***REMOVED***";
+    }
+}
---
+++ Storage/Local/FileSystem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIM.Storage.Local
+{
+    public class FileSystem : IStorage
+    {
+        #region Properties
+        public string ParentDirectory { get; set; }
+        #endregion
+        #region Constructor
+        public FileSystem(string parentDirectory)
+        {
+            ParentDirectory = parentDirectory;
+        }
+        #endregion
+        #region Methods
+        public Task AddObject(string ObjectName, Stream aStream)
+        {
+            throw new NotImplementedException();
+            //try
+            //{
+            //    string directory = Path.Combine(ParentDirectory, Path.GetDirectoryName(ObjectName));
+
+            //    if (!Directory.Exists(Path.Combine(directory)))
+            //        Directory.CreateDirectory(directory);
+
+            //    using (var fileStream = File.Create(Path.Combine(ParentDirectory, ObjectName)))
+            //    {
+            //        //reset stream position to 0 prior to copying to filestream;
+            //        aStream.Position = 0;
+            //        aStream.CopyTo(fileStream);
+            //    }//end using
+
+            //}
+            //catch (Exception)
+            //{
+            //    
+            //}
+        }
+        public Task<bool> DeleteObject(string ObjectName)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Stream> GetObject(string ObjectName)
+        {
+            throw new NotImplementedException();
+            //string objfile = Path.Combine(ParentDirectory, ObjectName);
+            //try
+            //{
+            //    return File.OpenRead(objfile);
+            //}
+            //catch (Exception)
+            //{
+            //    return null;
+            //}
+        }
+        #endregion
+        #region "Helper Methods"
+        private Boolean isDirectory(string filelocation)
+        {
+            try
+            {
+                FileAttributes attr = File.GetAttributes(filelocation);
+                if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                    return true;
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }//end try
+        }
+        #endregion
+    }
+}
