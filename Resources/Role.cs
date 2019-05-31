daabf9d1fa8af7df26ca6dba249a3c22b96a73dd
Jeremy Newson <jknewson@usgs.gov>
2019-05-31 06:55:39 -0600

    added role constants

---
+++ Resources/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIM.Resources
+{
+    public class Role
+    {
+        public const string Admin = "Administrator";
+        public const string Manager = "Manager";
+    }
+}
